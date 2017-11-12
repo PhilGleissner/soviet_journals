c06c5b20c43c6ab208f0adff57223ef2057b07d8
Rabea Gleissner <rabeagleissner@gmail.com>
2017-11-12 16:34:35 +0000

    Add visual essays sub nav

--- app.py
+++ app.py
@@ -8,13 +8,29 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
-@app.route('/visualizations/database')
-def database():
-	return render_template('visualizations/database.html')
-
 @app.route('/journals')
 def journals():
     return render_template('/journals.html', journals_data=JOURNALS_DATA)
+
+@app.route('/visual-essays/overview')
+def essays_overview():
+	return render_template('visual-essays/overview.html')
+
+@app.route('/visual-essays/essay-1')
+def essay_one():
+        return render_template('visual-essays/essay-1.html')
+
+@app.route('/visual-essays/essay-2')
+def essay_two():
+        return render_template('visual-essays/essay-2.html')
+
+@app.route('/visual-essays/essay-3')
+def essay_three():
+        return render_template('visual-essays/essay-3.html')
+
+@app.route('/visualizations/database')
+def database():
+	return render_template('visualizations/database.html')
 
 if __name__ == '__main__':
 	app.run()
